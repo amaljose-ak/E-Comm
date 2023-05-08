bfe5364f80b16f89e563d00c45782c9d3a6e58f1
amaljose-ak <amalkanchiyar@gmail.com>
2023-05-08 12:15:06 +0530

    added Login page and used validation

---
+++ routes/admin/adminLogin.js
@@ -0,0 +1,29 @@
+const router = require('express').Router()
+const adminModel = require('../../model/adminModel')
+const bcrypt = require('bcrypt')
+const {loginValidation}=require('../../function/admin/adminValidation')
+
+router.get('/all',async(req,res)=>{
+  const viewall= await adminModel.find()
+  res.json({viewall})
+})
+
+
+
+// login router
+router.post('/',async(req,res)=>{
+    const {error}=loginValidation(req.body)
+    if (error){
+        return res.status(500).json({message:error.details[0].message})
+    }
+    const adminExst= await adminModel.findOne({email:req.body.email})
+    if(!adminExst){
+        return res.status(500).json({message:"email is not matching"})
+    }
+    const validPasswrd= await bcrypt.compare(req.body.password,adminExst.password)
+    if(!validPasswrd){
+        return res.status(500).json({message:"password is mismatching"})
+    }
+    res.json({msg:'login succesfull'})
+})
+module.exports=router 
